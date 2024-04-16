efebf7f1c4814b8eaba34131f212e78acd4d74e6
zhanghui <hui.zhang@partners.sephora.cn>
2024-04-17 00:16:09 +0800

    payment

---
+++ pocket-payment-core/src/main/java/com/pocket/payment/App.java
@@ -0,0 +1,13 @@
+package com.pocket.payment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
